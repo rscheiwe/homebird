afb435c82897d1bd33bf94309d515b8e6a401743
Richard Scheiwe <richard.s@RICHARD-S-M1.local>
2023-02-07 22:30:38 -0500

    added models

--- api/homebird.py
+++ api/homebird.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Path
 from typing import List, Any
-
+from api.models import HomeSchema, HomeDB, HomeId
 from pysondb.db import JsonDatabase
 from api.create_db import create_db
 
@@ -9,7 +9,7 @@ router = APIRouter()
 homebird_db: JsonDatabase = create_db()
 
 
-@router.get("/homes")
+@router.get("/homes", response_model=List[HomeDB])
 async def read_all_homes() -> List:
     """
     Retrieve all homes.
@@ -21,7 +21,7 @@ async def read_all_homes() -> List:
     return data
 
 
-@router.get("/homes-ids")
+@router.get("/homes-ids", response_model=List[HomeId])
 async def read_all_homes_ids() -> List:
     """
     Retrieve list of home_ids for testing.
@@ -30,10 +30,13 @@ async def read_all_homes_ids() -> List:
         data = homebird_db.getAll()
     except:
         raise HTTPException(status_code=404, detail=f"Home IDs not found")
-    return [{key: value for key, value in obj.items() if key == "id"} for obj in data]
+    return [
+        {key: value for key, value in obj.items() if key == "id"}
+        for obj in data
+    ]
 
 
-@router.get("/homes/{home_id}")
+@router.get("/homes/{home_id}", response_model=HomeDB)
 async def read_home(
         home_id: int = Path(..., gt=0),
 ) -> Any:
--- api/models.py
+++ api/models.py
@@ -1,12 +1,48 @@
-from pydantic import BaseModel, Field, NonNegativeInt
-from datetime import datetime as dt
+from pydantic import BaseModel
 
 
-class NoteSchema(BaseModel):
-    title: str = Field(..., min_length=3, max_length=50)  # additional validation for the inputs
-    description: str = Field(..., min_length=3, max_length=50)
-    completed: str = "False"
+class HomeSchema(BaseModel):
+    air_conditioning: str
+    attic: str
+    basement: str
+    building_area_sq_ft: int
+    building_condition_score: int
+    building_quality_score: int
+    construction_type: str
+    exterior_walls: str
+    fireplace: str
+    full_bath_count: int
+    garage_parking_of_cars: int
+    garage_type_parking: str
+    heating: str
+    heating_fuel_type: str
+    no_of_buildings: int
+    no_of_stories: int
+    number_of_bedrooms: int
+    number_of_units: int
+    partial_bath_count: int
+    pool: str
+    property_type: str
+    roof_cover: str
+    roof_type: str
+    site_area_acres: float
+    style: str
+    total_bath_count: float
+    total_number_of_rooms: int
+    sewer: str
+    subdivision: str
+    water: str
+    year_built: int
+    zoning: str
 
 
-class NoteDB(NoteSchema):
+class HomeId(BaseModel):
     id: int
+
+
+class HomeDB(HomeSchema):
+    id: int
+
+
+
+
