fa48dac03ce3a88fb06ac5e85546905fc74fef9f
Richard Scheiwe <richard.s@RICHARD-S-M1.local>
2023-02-07 22:15:02 -0500

    homes up

--- api/homebird.py
+++ api/homebird.py
@@ -4,34 +4,41 @@ from typing import List, Any
 from pysondb.db import JsonDatabase
 from api.create_db import create_db
 
-from dummy_db.house_canary_data import house_one
-
-
 router = APIRouter()
+
+homebird_db: JsonDatabase = create_db()
 
 
 @router.get("/homes")
 async def read_all_homes():
-    # some async operation could happen here
-    # example: `notes = await get_all_notes()`
-    return {"health_check": "health status OK!"}
+    """
+    Retrieve all homes.
+    """
+    data = homebird_db.getAll()
+
+    if not data:
+        raise HTTPException(status_code=404, detail=f"Homes not found")
+    return data
+
+
+@router.get("/homes-ids")
+async def read_all_homes_ids():
+    """
+    Retrieve list of home_ids for testing.
+    """
+    data = homebird_db.getAll()
+    return [{key: value for key, value in obj.items() if key == "id"} for obj in data]
 
 
 @router.get("/homes/{home_id}")
 async def read_home(
-        home_id,
+        home_id: int = Path(..., gt=0),
 ) -> Any:
     """
     Retrieve home by ID.
     """
-
-    homebird_db = create_db()
-
-    if home_id:
-        q = {"garage_type_parking": "underground_basement"}
-        data = homebird_db.getById(313137202477298579)
-    # else:
-    #     home = None
-    if not data:
+    try:
+        data = homebird_db.getById(home_id)
+    except:
         raise HTTPException(status_code=404, detail=f"Home with id: {home_id} not found")
     return data
